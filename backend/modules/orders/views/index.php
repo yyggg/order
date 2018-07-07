33819fd34c1f3dced0315b1cb5d74260b3005396
老杨 <260101081@qq.com>
2018-07-07 11:49:07 +0800

    修改备注功能

--- backend/modules/orders/views/index.php
+++ backend/modules/orders/views/index.php
@@ -55,7 +55,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'class' => 'yii\grid\ActionColumn',
                 'header'=>'操作',
                 'template' => '{update} {view}',
-                'headerOptions'=> ['width'=> '155'],
+                'headerOptions'=> ['width'=> '95'],
                 'buttons' => [
                     'update' => function ($url, $model) {
                         if($model->status < 2)
